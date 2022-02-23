08f18664c6ff6832a9e7fb630317d80edf7a7e30
sper0054 <teresa.sperl@gmail.com>
2022-02-23 16:11:36 -0600

    end of 2

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -59,9 +59,13 @@ router.post('/login', (req, res) => {
             return;
         }
 
-        res.json({ user: dbUserData });
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
 
-        //Verify user
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
 });
 
