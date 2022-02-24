219442f7937867d288225f8fbbd5b7e7dda4a12d
sper0054 <teresa.sperl@gmail.com>
2022-02-23 20:03:48 -0600

    index many

--- models/index.js
+++ models/index.js
@@ -1,5 +1,18 @@
+const Vote = require('./Vote');
 const Post = require("./Post");
 const User = require('./User');
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -9,4 +22,20 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
