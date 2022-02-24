7a7f7b4da1f0e123df92be710df6181cc272b75a
sper0054 <teresa.sperl@gmail.com>
2022-02-24 11:19:29 -0600

    update attributes using seq

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,7 +5,9 @@ const sequelize = require('../../config/connection');
 // get all users
 router.get('/', (req, res) => {
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: ['id', 'post_url', 'title', 'created_at', 
+  [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+],
     order: [['created_at', 'DESC']],
     include: [
       {
@@ -26,7 +28,9 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: ['id', 'post_url', 'title', 'created_at', 
+  [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+],
     include: [
       {
         model: User,
