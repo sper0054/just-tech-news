dcf1e94fad0b07b7215e0610117f14459798564f
sper0054 <teresa.sperl@gmail.com>
2022-02-24 11:14:39 -0600

    more post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Vote, Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
@@ -65,11 +66,29 @@ router.put('/upvote', (req, res) => {
   Vote.create({
     user_id: req.body.user_id,
     post_id: req.body.post_id
-  })
-  .then(dbPostData => res.json(dbPostData))
-  .catch(err => res.json(err));
-});
-
+  }).then(() => {
+    return Post.findOne({
+      where: {
+        id: req.body.post_id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [
+          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id'),
+          'vote_count'
+        ]
+      ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  });
+    
 router.put('/:id', (req, res) => {
   Post.update(
     {
